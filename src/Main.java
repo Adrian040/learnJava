7db791a91d20cdd1bfde095eb06fd2a8ce3e6011
Adrian Maldonado <am96929@gmal.com>
2025-02-27 02:09:51 -0600

    Escape de los signos especiales, salto de linea y tabulación en Strings.

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,7 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "Hola Mundo!";
-        // Nota: Los Strings son inmutables!
-
-        int largo = text.length(); // Metodo del largo del string
-        System.out.println(largo);
-
-        String texto = text.replace("Hola", "Chao");
+        String texto = "C:\\Hola \"Mundo\">! \n Hola nueva linea \ttabulada";
         System.out.println(texto);
-
-        System.out.println(texto.endsWith("undo!"));
-        System.out.println(texto.startsWith("Ch"));
-        System.out.println(texto.contains("und"));
-        System.out.println(texto.indexOf("Mund"));  // Regresa el índice en el que empieza el argumneto. Regresa -1 si no lo encuentra
-
-        System.out.println(texto.toUpperCase());
-        System.out.println(texto.toLowerCase());
-
-        String malo = "   they're not like us   ";
-        System.out.println(malo);
-        System.out.println(malo.trim());   // Elimina los espacios en blanco al inicio y al final.
     }
 }
