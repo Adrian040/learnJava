b1cb0367897a633720f1211c83bb0bc7e552ba97
Adrian Maldonado <am96929@gmal.com>
2025-02-27 02:38:26 -0600

    final Strings (constantes, no se pueden actualizar.)

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] numeros2 = {{0,1},{2,3}};  // array de dos dimensiones definiendo explícitamente.
-        System.out.println(Arrays.deepToString(numeros2));  // Esto muestra el array
-        int[][][] numeros3 = new int[3][2][2]; // Array de tres dims definiéndolo como de ceros y luego reemplazando algunos de sus valores a continuación:
-        numeros3[0][0][0] = 5;
-        numeros3[2][1][1] = 10;
-        System.out.println(Arrays.deepToString(numeros3));
+        String empresa = "Hola mundo BBB";
+        empresa = "Hola mundo bb"; // Se actualiza el string empresa.
+        System.out.println(empresa);
+
+        final String empresa2 = "Hola mundo LL"; // Este es constante, ya no se puede actualizar más adelante.
+//        empresa2 = "Hola mundo ll"; // Esto arrojaría un error.
+        System.out.println(empresa2);
     }
 }
